dec7faae3b9a620e9e61bcbd436aaacc9f8d590d
Marcio <mpschiehl@outlook.com>
2018-05-21 09:56:28 -0300

    Inicialização do Repositorio

---
+++ ExemploNoisDois/src/Principal.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Márcio Pedro Schiehl and Ricardo Borba.
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Minha Mensagem");
+    }
+    
+}
